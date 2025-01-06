d4052843fbb0f8c3ea705d6769e86a589937f66c
akshaysingh29 <akshay.singh@uniproworld.com>
2025-01-06 12:55:28 +0530

    remove and update Collaborator functionality added

--- src/lib/actions/room.actions.ts
+++ src/lib/actions/room.actions.ts
@@ -2,7 +2,9 @@
 import { nanoid } from 'nanoid';
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
-import { parseStringify } from '../utils';
+import { getAccessType, parseStringify } from '../utils';
+import { redirect } from 'next/navigation';
+
 export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
     const roomId = nanoid();
     try {
@@ -60,6 +62,46 @@ export const getDocumentsList = async (email: string) => {
         return parseStringify(rooms);
     } catch (error) {
         console.log(`Error happened while getting rooms:${error}`);
+    }
+}
+
+export const updateDocumentAccess = async ({ roomId, email, userType }: ShareDocumentParams) => {
+    try {
+        const usersAccesses: RoomAccesses = {
+            [email]: getAccessType(userType) as AccessType
+        }
+        const room = await liveblocks.updateRoom(roomId, { usersAccesses })
+        if (room) {
+            //TODO: Send Notification to User
+        }
+        revalidatePath(`/documents/${roomId}`);
+        return parseStringify(room)
+    } catch (error) {
+        console.log(`Error happened while updating a room access ${error}`)
+    }
+}
+
+export const removeCollaborator = async ({ roomId, email }: { roomId: string, email: string }) => {
+    try {
+        const room = await liveblocks.getRoom(roomId);
+        if (room.metadata.email === email) {
+            throw new Error(`You cannot remove yourself from the document`)
+        }
+        const updateRoom = await liveblocks.updateRoom(roomId, { usersAccesses: { [email]: null } });
+        revalidatePath(`/documents/${roomId}`);
+        return parseStringify(updateRoom);
+    } catch (error) {
+        console.log(`Error happened while removing a collaborator: ${error}`)
+    }
+}
+
+export const deleteDocument = async (roomId: string) => {
+    try {
+        await liveblocks.deleteRoom(roomId);
+        revalidatePath(`/`);
+        redirect('/')
+    } catch (error) {
+        console.log(`Error happened while deleting a room: ${error}`)
     }
 }
 
